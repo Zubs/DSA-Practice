d9821add92e7b53840c029b763bed1a33785596f
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-02-19 14:34:55 +0100

    Fixed size arrays

--- src/array.php
+++ src/array.php
@@ -1,4 +1,13 @@
 <?php
 
-$array = new SplFixedArray(10);
-$array = range(1, 10);
+$array1 = new SplFixedArray(10);
+for ($i = 0; $i < 10; $i++) {
+    $array1[] = $i;
+}
+
+$dynamicArray = [
+    0 => 'Man',
+    1 => 'Woman',
+    2 => 'Boys'
+];
+$array2 = SplFixedArray::fromArray($dynamicArray);
