744851165d0c0125e0c3d6327a263a1f1a6df690
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2023-11-01 18:05:41 +0000

    feat: fixed TS QuickSort

--- ts/dist/QuickSort.js
+++ ts/dist/QuickSort.js
@@ -1,75 +1,75 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.partition = exports.QuickSort = void 0;
-var QuickSort = function (numberArray) {
-    if (numberArray.length <= 1) {
-        return numberArray;
+exports.SortableArray = void 0;
+var SortableArray = /** @class */ (function () {
+    function SortableArray(numberArray) {
+        this.numberArray = numberArray;
     }
-    var pivotIndex = (0, exports.partition)(numberArray, 0, numberArray.length - 1);
-    console.log({
-        pivotIndex: pivotIndex,
-        numberArray: numberArray,
-    });
-    // QuickSort(numberArray.slice(0, pivotIndex));
-    //
-    // QuickSort(numberArray.slice(pivotIndex + 1));
-    return numberArray;
-};
-exports.QuickSort = QuickSort;
-var partition = function (numberArray, leftPointer, rightPointer) {
-    // Use the rightmost element as the pivot
-    var pivotIndex = rightPointer;
-    var pivotValue = numberArray[pivotIndex];
-    // Use the element before the pivot as the right pointer
-    rightPointer -= 1;
-    while (true) {
-        /**
-         * STEP 1: The left pointer continuously moves one cell to the right until it reaches
-         * a value that is greater than or equal to the pivot, and then stops.
-         */
-        while (numberArray[leftPointer] < pivotValue) {
+    SortableArray.prototype.quicksort = function (leftIndex, rightIndex) {
+        if (rightIndex - leftIndex <= 0) {
+            return;
+        }
+        var pivotIndex = this.partition(leftIndex, rightIndex);
+        this.quicksort(leftIndex, pivotIndex - 1);
+        this.quicksort(pivotIndex + 1, rightIndex);
+        return this.numberArray;
+    };
+    SortableArray.prototype.partition = function (leftPointer, rightPointer) {
+        // Use the rightmost element as the pivot
+        var pivotIndex = rightPointer;
+        var pivotValue = this.numberArray[pivotIndex];
+        // Use the element before the pivot as the right pointer
+        rightPointer -= 1;
+        while (true) {
+            /**
+             * STEP 1: The left pointer continuously moves one cell to the right until it reaches
+             * a value that is greater than or equal to the pivot, and then stops.
+             */
+            while (this.numberArray[leftPointer] < pivotValue) {
+                leftPointer += 1;
+            }
+            /**
+             * STEP 2: Then, the right pointer continuously moves one cell to the left until it
+             * reaches a value that is less than or equal to the pivot, and then stops.
+             * The right pointer will also stop if it reaches the beginning of the array.
+             */
+            while (this.numberArray[rightPointer] > pivotValue) {
+                // Stop when at the beginning of the array
+                if (rightPointer === 0) {
+                    break;
+                }
+                rightPointer -= 1;
+            }
+            /**
+             * STEP 3: Once the right pointer has stopped, we reach a crossroads.
+             * If the left pointer has reached (or gone beyond) the right pointer,
+             * we move on to STEP 4.
+             */
+            if (leftPointer >= rightPointer) {
+                break;
+            }
+            else {
+                /**
+                 * STEP 3: Otherwise, we swap the values that the left and right pointers are
+                 * pointing to, and then go back to repeat Steps 1, 2, and 3 again.
+                 */
+                var temp_1 = this.numberArray[leftPointer];
+                this.numberArray[leftPointer] = this.numberArray[rightPointer];
+                this.numberArray[rightPointer] = temp_1;
+            }
+            // At this point, left and right pointer values have been swapped
+            // Increment the left pointer to restart the process
             leftPointer += 1;
         }
         /**
-         * STEP 2: Then, the right pointer continuously moves one cell to the left until it
-         * reaches a value that is less than or equal to the pivot, and then stops.
-         * The right pointer will also stop if it reaches the beginning of the array.
+         * STEP 4: Finally, we swap the pivot with the value that the left pointer is currently
+         * pointing to.
          */
-        while (numberArray[rightPointer] > pivotValue) {
-            // Stop when at the beginning of the array
-            if (rightPointer === 0) {
-                break;
-            }
-            rightPointer -= 1;
-        }
-        /**
-         * STEP 3: Once the right pointer has stopped, we reach a crossroads.
-         * If the left pointer has reached (or gone beyond) the right pointer,
-         * we move on to STEP 4.
-         */
-        if (leftPointer >= rightPointer) {
-            break;
-        }
-        else {
-            /**
-             * STEP 3: Otherwise, we swap the values that the left and right pointers are
-             * pointing to, and then go back to repeat Steps 1, 2, and 3 again.
-             */
-            var temp_1 = numberArray[leftPointer];
-            numberArray[leftPointer] = numberArray[rightPointer];
-            numberArray[rightPointer] = temp_1;
-        }
-        // At this point, left and right pointer values have been swapped
-        // Increment the left pointer to restart the process
-        leftPointer += 1;
-    }
-    /**
-     * STEP 4: Finally, we swap the pivot with the value that the left pointer is currently
-     * pointing to.
-     */
-    var temp = numberArray[leftPointer];
-    numberArray[leftPointer] = numberArray[pivotIndex];
-    numberArray[pivotIndex] = temp;
-    return leftPointer;
-};
-exports.partition = partition;
+        var temp = this.numberArray[leftPointer];
+        this.numberArray[leftPointer] = this.numberArray[pivotIndex];
+        this.numberArray[pivotIndex] = temp;
+        return leftPointer;
+    };
+    return SortableArray;
+}());
+exports.SortableArray = SortableArray;
--- ts/src/Array/QuickSort.ts
+++ ts/src/Array/QuickSort.ts
@@ -1,81 +1,84 @@
-export const QuickSort = (numberArray: number[]): number[] => {
-    if (numberArray.length <= 1) {
-        return numberArray;
+export class SortableArray {
+    private numberArray: number[];
+
+    constructor(numberArray: number[]) {
+        this.numberArray = numberArray;
     }
 
-    const pivotIndex = partition(numberArray, 0, numberArray.length - 1);
+    quicksort(leftIndex: number, rightIndex: number): number[] | void {
+        if (rightIndex - leftIndex <= 0) {
+            return;
+        }
 
-    console.log({
-        pivotIndex,
-        numberArray,
-    })
+        const pivotIndex = this.partition(leftIndex, rightIndex);
 
-    // QuickSort(numberArray.slice(0, pivotIndex));
-    //
-    // QuickSort(numberArray.slice(pivotIndex + 1));
+        this.quicksort(leftIndex, pivotIndex - 1);
 
-    return numberArray;
-};
+        this.quicksort(pivotIndex + 1, rightIndex);
 
-export const partition = (numberArray: number[], leftPointer: number, rightPointer: number): number => {
-    // Use the rightmost element as the pivot
-    let pivotIndex = rightPointer;
-    const pivotValue = numberArray[pivotIndex];
-    // Use the element before the pivot as the right pointer
-    rightPointer -= 1;
+        return this.numberArray;
+    }
 
-    while (true) {
-        /**
-         * STEP 1: The left pointer continuously moves one cell to the right until it reaches
-         * a value that is greater than or equal to the pivot, and then stops.
-         */
-        while (numberArray[leftPointer] < pivotValue) {
+    private partition(leftPointer: number, rightPointer: number): number {
+        // Use the rightmost element as the pivot
+        let pivotIndex = rightPointer;
+        const pivotValue = this.numberArray[pivotIndex];
+        // Use the element before the pivot as the right pointer
+        rightPointer -= 1;
+
+        while (true) {
+            /**
+             * STEP 1: The left pointer continuously moves one cell to the right until it reaches
+             * a value that is greater than or equal to the pivot, and then stops.
+             */
+            while (this.numberArray[leftPointer] < pivotValue) {
+                leftPointer += 1;
+            }
+
+            /**
+             * STEP 2: Then, the right pointer continuously moves one cell to the left until it
+             * reaches a value that is less than or equal to the pivot, and then stops.
+             * The right pointer will also stop if it reaches the beginning of the array.
+             */
+            while (this.numberArray[rightPointer] > pivotValue) {
+                // Stop when at the beginning of the array
+                if (rightPointer === 0) {
+                    break;
+                }
+
+                rightPointer -= 1;
+            }
+
+            /**
+             * STEP 3: Once the right pointer has stopped, we reach a crossroads.
+             * If the left pointer has reached (or gone beyond) the right pointer,
+             * we move on to STEP 4.
+             */
+            if (leftPointer >= rightPointer) {
+                break;
+            } else {
+                /**
+                 * STEP 3: Otherwise, we swap the values that the left and right pointers are
+                 * pointing to, and then go back to repeat Steps 1, 2, and 3 again.
+                 */
+                const temp = this.numberArray[leftPointer];
+                this.numberArray[leftPointer] = this.numberArray[rightPointer];
+                this.numberArray[rightPointer] = temp;
+            }
+
+            // At this point, left and right pointer values have been swapped
+            // Increment the left pointer to restart the process
             leftPointer += 1;
         }
 
         /**
-         * STEP 2: Then, the right pointer continuously moves one cell to the left until it
-         * reaches a value that is less than or equal to the pivot, and then stops.
-         * The right pointer will also stop if it reaches the beginning of the array.
+         * STEP 4: Finally, we swap the pivot with the value that the left pointer is currently
+         * pointing to.
          */
-        while (numberArray[rightPointer] > pivotValue) {
-            // Stop when at the beginning of the array
-            if (rightPointer === 0) {
-                break;
-            }
+        const temp = this.numberArray[leftPointer];
+        this.numberArray[leftPointer] = this.numberArray[pivotIndex];
+        this.numberArray[pivotIndex] = temp;
 
-            rightPointer -= 1;
-        }
-
-        /**
-         * STEP 3: Once the right pointer has stopped, we reach a crossroads.
-         * If the left pointer has reached (or gone beyond) the right pointer,
-         * we move on to STEP 4.
-         */
-        if (leftPointer >= rightPointer) {
-            break;
-        } else {
-            /**
-             * STEP 3: Otherwise, we swap the values that the left and right pointers are
-             * pointing to, and then go back to repeat Steps 1, 2, and 3 again.
-             */
-            const temp = numberArray[leftPointer];
-            numberArray[leftPointer] = numberArray[rightPointer];
-            numberArray[rightPointer] = temp;
-        }
-
-        // At this point, left and right pointer values have been swapped
-        // Increment the left pointer to restart the process
-        leftPointer += 1;
+        return leftPointer;
     }
-
-    /**
-     * STEP 4: Finally, we swap the pivot with the value that the left pointer is currently
-     * pointing to.
-     */
-    const temp = numberArray[leftPointer];
-    numberArray[leftPointer] = numberArray[pivotIndex];
-    numberArray[pivotIndex] = temp;
-
-    return leftPointer;
-};
+}
